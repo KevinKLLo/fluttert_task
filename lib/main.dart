8e5506471b178e381f4b03e14b20a7d379731168
Kevin Lo <kevin.lo@pinkoi.com>
2023-03-10 23:54:32 +0800

    [Feat] set Card color

--- lib/main.dart
+++ lib/main.dart
@@ -67,9 +67,13 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(pair.asLowerCase),
+    var theme = Theme.of(context);
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(pair.asLowerCase),
+      ),
     );
   }
 }
